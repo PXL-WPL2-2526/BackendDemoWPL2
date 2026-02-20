622739e09f6dc0ac7e414504d0ce986a461313f6
Sander De Puydt <pxldemo@gmail.com>
2026-02-20 13:12:59 +0100

    project created

---
+++ DemoWPL2/Controllers/StudentsController.cs
@@ -0,0 +1,25 @@
+﻿using Demo.Application.Services;
+using Demo.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly StudentService _service;
+
+        public StudentsController()
+        {
+            _service = new StudentService();
+        }
+
+        [HttpGet]
+        public ActionResult<List<Student>> Get()
+        {
+            return Ok(_service.GetStudents());
+        }
+    }
+}
